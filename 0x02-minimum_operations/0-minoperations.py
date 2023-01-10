dea9dad431c049d5e749e22d7718acc59e537b7e
kabingusam <kabingusammy@gmail.com>
2023-01-10 15:50:09 +0300

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,18 +8,29 @@ def minOperations(n: int) -> int:
     :param n: the number of Hs we want to reach in the text file
     :return: returns the number of operations needed to reach n Hs, 0 if n <= 1 or n is not int
     '''
-    if n <= 0:
+    if not isinstance(n, int):
         return 0
-    steps = 0
-    while n > 1:
-        i = 2
-        while i * i <= n:
-            if n % i == 0:
-                n = n // i
-                steps += i
-                break
-            i += 1
-        if i * i > n:
-            n = n - 1
-            steps += 1
-    return steps
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
